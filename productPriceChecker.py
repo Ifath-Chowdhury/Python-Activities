42c81d20519e7ff2b46a744dbcf29a1ad15d5e87
ir2-chowdhury <Ifath2.Chowdhury@live.uwe.ac.uk>
2025-05-19 23:05:07 +0100

    Made simple code to output different string depending on which of two variables is higher

---
+++ productPriceChecker.py
@@ -0,0 +1,11 @@
+# Product Price Checker
+
+# Defining variables
+apple = 300
+banana = 500
+
+# If statement to print a different output depending on which value is higher
+if apple > banana:
+    print("Banana is less expensive")
+else:
+    print("Apple is less expensive")
