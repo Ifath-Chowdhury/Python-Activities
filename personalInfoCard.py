c25db2e2fa2510365165352370d6032afa442e9a
ir2-chowdhury <Ifath2.Chowdhury@live.uwe.ac.uk>
2025-05-19 23:24:21 +0100

    Created personal info card with formatted string

---
+++ personalInfoCard.py
@@ -0,0 +1,10 @@
+# 3.7 Project: Personal Information Card
+
+name = "Dave"
+age = 43
+city = "New York"
+hobby = "Collecting rocks"
+
+card = "Name: {name}\nAge: {age}\nCity: {city}\nHobby: {hobby}"
+
+print(card.format(name = name, age = age, city = city, hobby = hobby))
